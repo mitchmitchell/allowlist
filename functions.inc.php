afd030b0980649609f5653afa22514dade23579a
James Finstrom <jfinstrom@clearlyip.com>
2021-02-26 08:01:43 +0000

    Minor cleanup. Add git ignore

--- functions.inc.php
+++ functions.inc.php
@@ -100,7 +100,8 @@ function allowlist_hookGet_config($engine) {
 					$cidnum = trim($item['cidnum']);
 
 					if (allowlist_did_get($exten . "/" . $cidnum)) {
-						var_dump($exten, $cidnum, $item);
+						
+						dbug($exten, $cidnum, $item);
 						if ($cidnum != '' && $exten == '') {
 							$exten = 's';
 							$pricid = ($item['pricid']) ? true:false;
